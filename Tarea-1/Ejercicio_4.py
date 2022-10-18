99c481b92d5470b116b6d36b5d48243f35b9453f
Luis Mario <luismescobar@ciencias.unam.mx>
2022-10-17 20:03:00 -0500

    Bfs corregido

--- Tarea-1/Ejercicio_4.py
+++ Tarea-1/Ejercicio_4.py
@@ -1,3 +1,4 @@
+from audioop import reverse
 import pandas as pd
 import sys
 from collections import deque
@@ -12,15 +13,23 @@ df = pd.DataFrame()
 df = pd.read_csv('metro.csv')
 df = df.iloc[: , 1:]
 
-metro = df['origen'].tolist()
+metro = df['origen'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8').tolist()
 metro = [*set(metro)]
 
-result = [(x,y,z) for x,y,z in zip(df['origen'], df['destino'], df['longitud'])]
-print(len(metro))
+result = [(x,y,z) for x,y,z in zip(df['origen'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'), 
+                                   df['destino'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'),
+                                   df['longitud'])]
 
 aux = {i:[] for i in metro}
+
 for i in result:
     aux[i[0]].append((i[1], i[2]))
+
+
+hijos ={j:[] for j in metro}
+
+for j in result:
+    hijos[j[0]].append(j[1])
 
 def distances(point1,point2):
 # approximate radius of earth in km
@@ -46,11 +55,11 @@ dl = pd.read_csv('LineasMetro.csv')
 dl = dl.iloc[: , 1:5]
 linea = dl['nombre'].tolist()
 linea = [*set(linea)]
-result = [(w,x,y,z) for  w,x,y,z in zip(dl['linea'],dl['nombre'], dl['lat'], dl['lon'])]
+r1 = [(w,x,y,z) for  w,x,y,z in zip(dl['linea'],dl['nombre'], dl['lat'], dl['lon'])]
 
 def busca_destino(nombre):
     cons = (None,None,None,None)
-    for r in result :
+    for r in r1 :
         if r[1] == nombre:
             cons = r
             break
@@ -58,7 +67,7 @@ def busca_destino(nombre):
 
 coordenadas = {}
 i = 0
-for r in result :
+for r in r1 :
     if r[1] not in coordenadas :
         coordenadas[r[1]]  =(r[2],r[3])
     else:
@@ -76,10 +85,59 @@ for k in coordenadas:
         continue
 #print(estimated_cost)
 
-def mejor_seleccion(minimo,hijos,costos,ruta):
-    return
-def best_first_search(origen,destino):
-    return 
+def minimu(lista_hijos,cost):
+    nombre = lista_hijos[0]
+    minimo = cost[lista_hijos[0]]
+    for h in lista_hijos:
+        if  cost[h] < minimo:
+            nombre =  h
+            minimo = cost[h]
+    return nombre
+
+
+def mejor_seleccion(hijos,costos,ruta):
+    nombre = hijos[0]
+    #en la lista de hijos rescatar, sus costos y quedarse con el de menor costo
+    #ver si el hijo ya esta en la ruta , si el hijo con el costo minimo no esta , lo agregamos a la ruta y actualizamos 
+    #regresamos el nombre del hijo que se eligio para continuar con el trayecto 
+    minimo =  costos[hijos[0]]
+    for h in hijos :
+       # minimo = costos[hijos[0]]
+        if costos[h] < minimo and h not in ruta:
+            nombre = h
+            minimo = costos[h]
+    return nombre
+
+
+
+
+def best_first_search(origen,destino,costos):
+    ruta = [origen]
+    encontrado = False
+    guarda = origen
+    trayectorias = hijos[guarda]
+    while len(trayectorias) and encontrado!= True:
+        if destino in ruta :
+            encontrado = True
+            break
+        guarda = mejor_seleccion(trayectorias,costos,ruta)
+        #print("Hola")
+       # print(guarda)
+        trayectorias = hijos[guarda]
+        if destino in trayectorias:
+            ruta.append(guarda)
+            break
+        ruta.append(guarda)
+    #print(ruta)
+    ruta.append(destino)
+    ruta.reverse()
+
+    return ruta
+#print(busca_destino("Lazaro Cardenas"))
+#print(busca_destino("Lázaro Cárdenas"))
+#print(hijos["Lázaro Cardenas"])
+r = best_first_search("Lazaro Cardenas","Coyoacan",estimated_cost)
+print(r) 
 
 
 
